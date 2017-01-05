26bd59cfefc3e51d563be8f14e14376886510562
ssweeney2 <sean.sweeney@ed.ac.uk>
2017-01-05 12:36:48 +0000

    Update badgeleaderboard.sql

--- badgeleaderboard.sql
+++ badgeleaderboard.sql
@@ -1,4 +1,4 @@
-Badge Leaderboard for course visited (from) ###
+Badge Leaderboard for current course ###
 
 SELECT u.username, (SELECT COUNT(*) FROM prefix_badge_issued AS d WHERE d.userid = u.id) AS earned
 FROM prefix_role_assignments AS ra
