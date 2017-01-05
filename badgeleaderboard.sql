feb7b4fc8ac2f570526d598b6721da071ad7eb38
ssweeney2 <sean.sweeney@ed.ac.uk>
2017-01-05 12:02:04 +0000

    Add files via upload

---
+++ badgeleaderboard.sql
@@ -0,0 +1,8 @@
+Badge Leaderboard for course visited (from) ###
+
+SELECT u.username, (SELECT COUNT(*) FROM prefix_badge_issued AS d WHERE d.userid = u.id) AS earned
+FROM prefix_role_assignments AS ra
+JOIN prefix_context AS context ON context.id = ra.contextid
+JOIN prefix_course AS c ON c.id = context.instanceid AND c.id = %%COURSEID%%
+JOIN prefix_user AS u ON u.id = ra.userid
+ORDER BY earned DESC, u.username ASC
